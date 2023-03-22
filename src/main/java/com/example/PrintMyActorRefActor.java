264ef1dffa8e66fd53deb76b6f5bdd5c7a5f878b
Jevan Wu <jevanwu@gmail.com>
2023-03-22 17:11:28 +0800

    init

---
+++ src/main/java/com/example/PrintMyActorRefActor.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+public class PrintMyActorRefActor extends AbstractBehavior<String> {
+
+    public PrintMyActorRefActor(ActorContext<String> context) {
+        super(context);
+    }
+
+    public static Behavior<String> create() {
+        return Behaviors.setup(PrintMyActorRefActor::new);
+    }
+
+    @Override
+    public Receive<String> createReceive() {
+        return newReceiveBuilder().onMessageEquals("printit", this::printIt).build();
+    }
+
+    private Behavior<String> printIt() {
+        ActorRef<String> secondRef = getContext().spawn(Behaviors.empty(), "second-actor");
+        System.out.println("Second: " + secondRef);
+        return this;
+    }
+}
